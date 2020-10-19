b4dd516d94fecc7996e0586fd39117db5700eb2d
TheDavidGame <vlad.billI@bk.ru>
2020-10-19 17:19:07 +0300

    Initial commit

---
+++ src/sample/calcmain/Calc.java
@@ -0,0 +1,8 @@
+package sample.calcmain;
+
+public class Calc  {
+    public static void main(String[] args) {
+        Controller calculator = new Controller();
+        calculator.thisStart();
+    }
+}
